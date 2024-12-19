04960b009964787e2b2bdb26558185e1ed6c46d0
RanaFaiz <rfaiz6537@gmail.com>
2024-12-19 19:20:09 +0500

    aadsfdas

--- src/store/reducers/User.js
+++ src/store/reducers/User.js
@@ -29,6 +29,8 @@ export const registerUser = createAsyncThunk(
   }
 );
 
+funct
+
 export const updateUserProfile = createAsyncThunk(
   "auth/updateUserProfile",
   async (values, { rejectWithValue }) => {
